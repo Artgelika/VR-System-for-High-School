e2a2d293bfd115ee4142140bb1c992df4d585310
Artgelika <Artgelika@gmail.com>
2021-11-15 19:45:04 +0100

    Add Hands

---
+++ Biochemical Lab/Assets/_Scripts/HandPresence.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public bool showController = false;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public GameObject handModelPrefab;
+    public List<GameObject> controllerPrefabs;
+
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+    private InputDevice targetDevice;
+
+
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if(prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else
+            {
+                Debug.Log("Did not find corresponding controller model");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+        } 
+    }
+
+
+    void Update()
+    {
+        if(showController)
+        {
+            spawnedHandModel.SetActive(false);
+            spawnedController.SetActive(true);
+        }
+        else
+        {
+            spawnedHandModel.SetActive(true);
+            spawnedController.SetActive(false);
+        }
+    }
+}
