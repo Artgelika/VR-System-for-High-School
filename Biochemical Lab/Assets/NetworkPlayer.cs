da05d6b4257c84109438da7d449418b441817018
Angelika Przeliorz <artgelika@gmail.com>
2022-09-15 17:04:34 +0200

    Add new network player prefab

--- Biochemical Lab/Assets/NetworkPlayer.cs
+++ Biochemical Lab/Assets/NetworkPlayer.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 using Photon.Pun;
+using Unity.XR.CoreUtils;
 
 public class NetworkPlayer : MonoBehaviour
 {
@@ -9,10 +11,18 @@ public class NetworkPlayer : MonoBehaviour
     public Transform rightHand;
     private PhotonView photonView;
 
+    private Transform headRig;
+    private Transform leftHandRig;
+    private Transform rightHandRig;
+
     // Start is called before the first frame update
     void Start()
     {
         photonView = GetComponent<PhotonView>();
+        XROrigin rig = FindObjectOfType<XROrigin>();
+        headRig = rig.transform.Find("Camera Offset/Main Camera");
+        leftHandRig = rig.transform.Find("Camera Offset/Left Hand");
+        rightHandRig = rig.transform.Find("Camera Offset/Right Hand");
     }
 
     // Update is called once per frame
@@ -24,19 +34,16 @@ public class NetworkPlayer : MonoBehaviour
             leftHand.gameObject.SetActive(false);
             head.gameObject.SetActive(false);
 
-            MapPosition(head, XRNode.Head);
-            MapPosition(leftHand, XRNode.LeftHand);
-            MapPosition(rightHand, XRNode.RightHand);
+            MapPosition(head, headRig);
+            MapPosition(leftHand, leftHandRig);
+            MapPosition(rightHand, rightHandRig);
         }
         
     }
 
-    void MapPosition(Transform target, XRNode node)
+    void MapPosition(Transform target, Transform rigTransform)
     {
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
-
-        target.position = position;
-        target.rotation = rotation;
+        target.position = rigTransform.position;
+        target.rotation = rigTransform.rotation;
     }
 }
